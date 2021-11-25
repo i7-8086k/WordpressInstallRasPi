5c2169f6a05a128dca165614a9c4cb3577a26e4c
Mattias Sträng <mattias.strang@jgy.se>
2021-11-25 09:05:09 +0100

    Update RasPiKiosk.sh

--- RasPiKiosk.sh
+++ RasPiKiosk.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 sudo apt-get update && sudo apt-get upgrade -y
 
-sudo nano /etc/crontab
-
 echo '0 3 * * * /sbin/shutdown -r now' | sudo tee -a /etc/crontab
 
-sudo nano /home/pi/.xinitrc
+sudo apt-get install unclutter
 
-echo '@xset s off
+sudo service lightdm restart
+
+echo '@lxpanel --profile LXDE
+@pcmanfm --desktop --profile LXDE
+@xset s off
 @xset -dpms
 @xset s noblank
-@chromium-browser –kiosk https://i7-8086k.github.io/Kiosk/' | sudo tee -a sudo nano /home/pi/.xinitrc
+@chromium-browser –kiosk https://i7-8086k.github.io/Kiosk/
 
+@/home/pi/run.sh' | sudo tee -a /etc/xdg/lxsession/LXDE-pi/autostart
+
+touch /home/pi/run.sh
+
+chmod +x /home/pi/run.sh
