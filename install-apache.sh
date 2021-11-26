a17a6bca47280bca7efd8038cddadef3bab607c9
Mattias Sträng <70257905+i7-8086k@users.noreply.github.com>
2021-11-26 08:24:21 +0100

    update

--- install-apache.sh
+++ install-apache.sh
@@ -9,6 +9,12 @@ sudo apt-get install php -y
 
 sudo apt-get install php-mysql -y
 
+# Rewrite för länkar ska fungera korrekt i wordpress
 sudo a2enmod rewrite
 
-sudo service apache2 restart
+# Lägger till texten på första raden.
+sudo sed -i '1i <Directory /var/www/html>\n\tOptions Indexes FollowSymLinks MultiViews\n\tAllowOverride All\n\tRequire all granted\n</Directory>\n\n' /etc/apache2/sites-available/000-default.conf
+
+# Startar om apache service, måste köras för att nya apache.conf ska gälla.
+sudo systemctl restart apache2
+
